4a01450e240a9ee41c5e5c3721f0270f25e85c75
Yakov <93822070+potapovjakov@users.noreply.github.com>
2023-05-26 20:47:35 +0300

    Add Truck model

--- delivery/models.py
+++ delivery/models.py
@@ -45,3 +45,34 @@ class Cargo(models.Model):
         verbose_name_plural = 'Грузы'
         ordering = ('-created_at',)
 
+
+class Truck(models.Model):
+    """
+    Машина
+    """
+    number = models.CharField(
+        'Номер',
+        max_length=5,
+        unique=True,
+    )
+    location = models.CharField(
+        'Индекс текущего месторасположения автомобиля',
+        max_length=5,
+    )
+    load_capacity = models.IntegerField(
+        'Грузоподъемность',
+        validators=[
+            MinValueValidator(
+                1,
+                message='Грузоподъемность должна быть больше 0',
+            ),
+            MaxValueValidator(
+                1000,
+                message='Грузоподъемность не может превышать 1000',
+            )
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Машина'
+        verbose_name_plural = 'Машины'
