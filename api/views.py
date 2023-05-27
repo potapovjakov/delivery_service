5703f563bf7958d040ff9581261dee2d1ef071ae
Yakov <93822070+potapovjakov@users.noreply.github.com>
2023-05-27 19:30:29 +0300

    add views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, status, viewsets
+from api.serializers import TruckSerializer, CargoSerializer
+from delivery.models import Truck, Cargo
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class TruckViewSet(viewsets.ModelViewSet):
+    queryset = Truck.objects.all()
+    serializer_class = TruckSerializer
+
+
+class CargoViewSet(viewsets.ModelViewSet):
+    queryset = Cargo.objects.all()
+    serializer_class = CargoSerializer
+
