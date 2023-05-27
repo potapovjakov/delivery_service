103786b8c898a7e4e4980ed53aaab87b66a171a0
Yakov <93822070+potapovjakov@users.noreply.github.com>
2023-05-27 14:58:19 +0300

    add insert trucks to db

--- utils/initial_script.py
+++ utils/initial_script.py
@@ -1,14 +1,17 @@
 import os
+import random
+import string
+
+import pandas as pd
 import psycopg2
 import psycopg2.extras as extras
 from dotenv import load_dotenv
-import pandas as pd
+
 import custom_logger
-
-
 
 load_dotenv()
 base_dir = os.path.dirname(os.path.abspath(__file__))
+
 
 logger = custom_logger.get_logger(__name__)
 
@@ -22,6 +25,11 @@ conn = psycopg2.connect(
 
 
 def csv_to_db(conn):
+    """
+    Импортирует локации из CSV файла в БД
+    :param conn:
+    :return:
+    """
     table = 'delivery_location'
     file_path = os.path.join(base_dir, 'uszips.csv')
     df = pd.read_csv(file_path)
@@ -35,7 +43,7 @@ def csv_to_db(conn):
     try:
         extras.execute_values(cursor, query, locations)
         conn.commit()
-        logger.info(f'{len(locations)} locations is inserted to table {table}')
+        logger.info(f'{len(locations)} locations is inserted to {table}')
 
     except (Exception, psycopg2.DatabaseError) as error:
         logger.exception(f'Error: {error}')
@@ -44,5 +52,60 @@ def csv_to_db(conn):
         cursor.close()
 
 
+def add_trucks(conn):
+    """
+    Добавляет 20 рандомных автомобилей в БД
+    :param conn:
+    :return:
+    """
+    query = """SELECT zip_code FROM delivery_location"""
+    cursor = conn.cursor()
+    cursor.execute(query)
+    zip_codes = cursor.fetchall()
+
+    truck_numbers = []
+
+    def generate_truck_number():
+        """
+        Генерирует уникальный номер автомобиля
+        :return:
+        """
+        integer = random.randint(1000, 9999)
+        liter = random.choice(string.ascii_uppercase)
+        number = f'{integer}{liter}'
+        if number not in truck_numbers:
+            truck_numbers.append(number)
+            return number
+        else:
+            generate_truck_number()
+
+    for truck in range(20):
+        truck_number = generate_truck_number()
+        current_location = random.choice(zip_codes)[0]
+        load_capacity = random.randint(1, 1000)
+        try:
+            cursor.execute(
+                """INSERT INTO delivery_truck (
+                truck_number,
+                current_location_id,
+                load_capacity
+                )
+                VALUES (%s, %s, %s)""", (
+                    truck_number,
+                    current_location,
+                    load_capacity
+                )
+            )
+            logger.debug(f'Inserted truck {truck_number} to db')
+        except (Exception, psycopg2.DatabaseError) as error:
+            logger.exception(f'Error: {error}')
+            conn.rollback()
+
+    conn.commit()
+    cursor.close()
+    logger.info(f'{len(truck_numbers)} trucks inserted to db')
+
+
 if __name__ == '__main__':
     csv_to_db(conn)
+    add_trucks(conn)
