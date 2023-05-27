7fc2824620b32a313ff8ab55db431d2ba7d2f121
Yakov <93822070+potapovjakov@users.noreply.github.com>
2023-05-28 02:03:04 +0300

    add logics fields to serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,10 @@
-from delivery.models import Truck, Cargo
+from delivery.models import Truck, Cargo, Location
 from rest_framework import serializers
+from delivery.services import get_all_trucks
 
 
 class TruckSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Truck
         fields = (
@@ -14,6 +16,11 @@ class TruckSerializer(serializers.ModelSerializer):
 
 
 class CargoSerializer(serializers.ModelSerializer):
+    nearest_trucks = serializers.SerializerMethodField(
+        read_only=True,
+        method_name='get_nearest_trucks_count',
+    )
+
     class Meta:
         model = Cargo
         fields = (
@@ -23,5 +30,61 @@ class CargoSerializer(serializers.ModelSerializer):
             'weight',
             'created_at',
             'updated_at',
-            'description'
+            'description',
+            'nearest_trucks'
+        )
+
+    def get_nearest_trucks_count(self, obj) -> int:
+        """
+        Возвращает количество машин, находящихся
+        не далее чем в 450 милях от точки загрузки
+        :param obj:
+        :return:
+        """
+        dict_trucks = get_all_trucks(Location, obj)
+        count = 0
+        for k, v in dict_trucks.items():
+            if v <= 450:
+                count += 1
+
+        return count
+
+
+class CargoDetailSerializer(serializers.ModelSerializer):
+    all_trucks = serializers.SerializerMethodField(
+        read_only=True,
+        method_name='get_all_trucks',
+    )
+    class Meta:
+        model = Cargo
+        fields = (
+            'id',
+            'pick_up',
+            'delivery',
+            'weight',
+            'created_at',
+            'updated_at',
+            'description',
+            'all_trucks'
+        )
+
+    def get_all_trucks(self, obj) -> dict:
+        """
+        Возвращает словарь со всеми машинами отсортированный по
+        увеличению расстояния до места загрузки
+        :param obj:
+        :return:
+        """
+        dict_trucks = get_all_trucks(Location, obj)
+        sorted_tuples = sorted(dict_trucks.items(), key=lambda item: item[1])
+        sorted_dict = {k: v for k, v in sorted_tuples}
+        return sorted_dict
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = (
+            'lat',
+            'lng',
         )
