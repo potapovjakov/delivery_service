d35c3bc44066bd75958faa59cc303efa6e186973
Yakov <93822070+potapovjakov@users.noreply.github.com>
2023-05-28 09:55:01 +0300

    add get_serializer_class to views

--- api/views.py
+++ api/views.py
@@ -1,15 +1,21 @@
 from rest_framework import generics, viewsets
 from api.serializers import TruckSerializer, CargoSerializer, \
-    LocationSerializer, CargoDetailSerializer
+    LocationSerializer, CargoDetailSerializer, CargoSerializerWithoutField, \
+    TruckSerializerWithoutField
 from delivery.models import Truck, Cargo, Location
 
 
 class TruckViewSet(viewsets.ModelViewSet):
     queryset = Truck.objects.all()
     serializer_class = TruckSerializer
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+        if self.request.method == 'PUT':
+            serializer_class = TruckSerializerWithoutField
+        return serializer_class
 
 
-class CargoViewSet(viewsets.ModelViewSet):
+class CargoViewSet(generics.ListCreateAPIView):
     queryset = Cargo.objects.all()
     serializer_class = CargoSerializer
 
@@ -18,8 +24,13 @@ class CargoDetailViewSet(generics.RetrieveUpdateAPIView):
     queryset = Cargo.objects.all()
     serializer_class = CargoDetailSerializer
 
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+        if self.request.method == 'PUT':
+            serializer_class = CargoSerializerWithoutField
+        return serializer_class
+
 
 class LocationDetailViewSet(generics.RetrieveAPIView):
     serializer_class = LocationSerializer
     queryset = Location.objects.all()
-
